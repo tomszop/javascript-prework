aeaee15e5b4bd0739e4888106eafd8b32911dcfc
Tomasz Szopa <tomszop@gmail.com>
2022-04-08 17:17:41 +0200

    Update JS variables

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 
-function playGame(playerInput){ 
+{const playGame=function(playerInput){ 
     clearMessages();
-
-    function getMoveName(argMoveId){
-        
+    const getMoveName=function(argMoveId){
         if(argMoveId == 1){
         return 'kamień';
         }
@@ -14,35 +12,26 @@ function playGame(playerInput){
         return 'nożyce';
         }
     }
-
-    function displayResult(argComputerMove, argPlayerMove){
-
+    const displayResult=function(argComputerMove, argPlayerMove){
         console.log('move:', argComputerMove, argPlayerMove);
-        
         if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
             printMessage('Wygrałeś!');
             }
-
         if( argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
         printMessage('Przegrałeś!');
         }
-    
         if( argComputerMove == argPlayerMove ){
         printMessage('Remis!');
         }
-    
         if( argComputerMove == 'papier' && argPlayerMove == 'kamień'){
         printMessage('Przegrałeś!');
         }
-    
         if( argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
         printMessage('Wygrałeś');
         }
-        
         if( argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
         printMessage('Przegrałeś!');
         }
-    
         if( argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
         printMessage('Wygrałeś!');
         } 
@@ -50,33 +39,25 @@ function playGame(playerInput){
     
     //Ruch Komputera
 
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
     console.log('Wylosowana liczba to: ' + randomNumber);
-
-
-    let argComputerMove = getMoveName(randomNumber);
-
+    
+    const argComputerMove = getMoveName(randomNumber);
     printMessage('Mój ruch to: ' + argComputerMove);
 
     //Ruch człowieka
 
-    let argPlayerMove = getMoveName(playerInput);
-
+    const argPlayerMove = getMoveName(playerInput);
     printMessage('Twój ruch to: ' + argPlayerMove);
-
     displayResult(argComputerMove, argPlayerMove);
-
-
 }
 document.getElementById('play-rock').addEventListener('click', function (){      
     playGame(1);    
-  });
-    
+  }); 
   document.getElementById('play-paper').addEventListener('click', function (){   
     playGame(2);
   });
   document.getElementById('play-scissors').addEventListener('click', function(){ 
     playGame(3);
   });
-
+}
